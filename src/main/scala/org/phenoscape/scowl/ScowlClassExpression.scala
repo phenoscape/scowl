4538fe831a96ab6453e7a734d6f8ef2079ca6b30
Jim Balhoff <balhoff@nescent.org>
2014-02-07 20:13:57 -0500

    Added union expressions.

--- src/main/scala/org/phenoscape/scowl/ScowlClassExpression.scala
+++ src/main/scala/org/phenoscape/scowl/ScowlClassExpression.scala
@@ -5,19 +5,16 @@ import org.semanticweb.owlapi.model.OWLClassExpression
 import org.semanticweb.owlapi.model.OWLObjectIntersectionOf
 import org.semanticweb.owlapi.model.OWLSubClassOfAxiom
 import org.semanticweb.owlapi.model.OWLEquivalentClassesAxiom
+import org.semanticweb.owlapi.model.OWLObjectUnionOf
 
 class ScowlClassExpression(val classExpression: OWLClassExpression) {
 
-  def and(other: OWLClassExpression): OWLObjectIntersectionOf = {
-    OWLManager.getOWLDataFactory().getOWLObjectIntersectionOf(classExpression, other);
-  }
+  def and(other: OWLClassExpression): OWLObjectIntersectionOf = OWLManager.getOWLDataFactory().getOWLObjectIntersectionOf(classExpression, other)
 
-  def SubClassOf(other: OWLClassExpression): OWLSubClassOfAxiom = {
-    OWLManager.getOWLDataFactory().getOWLSubClassOfAxiom(classExpression, other);
-  }
+  def or(other: OWLClassExpression): OWLObjectUnionOf = OWLManager.getOWLDataFactory().getOWLObjectUnionOf(classExpression, other)
 
-  def EquivalentTo(other: OWLClassExpression): OWLEquivalentClassesAxiom = {
-    OWLManager.getOWLDataFactory().getOWLEquivalentClassesAxiom(classExpression, other);
-  }
+  def SubClassOf(other: OWLClassExpression): OWLSubClassOfAxiom = OWLManager.getOWLDataFactory().getOWLSubClassOfAxiom(classExpression, other)
+
+  def EquivalentTo(other: OWLClassExpression): OWLEquivalentClassesAxiom = OWLManager.getOWLDataFactory().getOWLEquivalentClassesAxiom(classExpression, other)
 
 }
