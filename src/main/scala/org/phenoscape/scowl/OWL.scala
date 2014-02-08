1c0b608ae51392db70cd4b92df835220cf6e9523
Jim Balhoff <balhoff@nescent.org>
2014-02-07 20:28:55 -0500

    Converted many implicits to value classes. Should increase performance.

--- src/main/scala/org/phenoscape/scowl/OWL.scala
+++ src/main/scala/org/phenoscape/scowl/OWL.scala
@@ -14,6 +14,23 @@ import org.semanticweb.owlapi.model.OWLAxiom
 import org.semanticweb.owlapi.model.OWLNamedObject
 import org.semanticweb.owlapi.model.OWLAnnotationSubject
 import org.semanticweb.owlapi.model.OWLOntology
+import org.semanticweb.owlapi.model.OWLSubClassOfAxiom
+import org.semanticweb.owlapi.model.OWLObjectIntersectionOf
+import org.semanticweb.owlapi.model.OWLObjectUnionOf
+import org.semanticweb.owlapi.model.OWLEquivalentClassesAxiom
+import org.semanticweb.owlapi.model.OWLAnnotationAssertionAxiom
+import org.semanticweb.owlapi.model.OWLAnnotationProperty
+import org.semanticweb.owlapi.model.OWLAnnotationValue
+import org.semanticweb.owlapi.model.OWLObjectPropertyAssertionAxiom
+import org.semanticweb.owlapi.model.OWLClassAssertionAxiom
+import org.semanticweb.owlapi.model.OWLObjectHasValue
+import org.semanticweb.owlapi.model.OWLObjectExactCardinality
+import org.semanticweb.owlapi.model.OWLObjectMaxCardinality
+import org.semanticweb.owlapi.model.OWLObjectMinCardinality
+import org.semanticweb.owlapi.model.OWLObjectSomeValuesFrom
+import org.semanticweb.owlapi.model.OWLObjectPropertyExpression
+import org.semanticweb.owlapi.model.OWLObjectAllValuesFrom
+import org.semanticweb.owlapi.model.OWLSubPropertyChainOfAxiom
 
 object OWL {
 
@@ -35,18 +52,83 @@ object OWL {
 
   def ObjectProperty(iri: String): OWLObjectProperty = ObjectProperty(IRI.create(iri))
 
-  implicit def OWLObjectPropertyToProperty(value: OWLObjectProperty) = new ScowlObjectProperty(value)
-
-  implicit def OWLClassExpressionToClassExpression(value: OWLClassExpression) = new ScowlClassExpression(value)
-
-  implicit def OWLIndividualToIndividual(value: OWLIndividual) = new ScowlIndividual(value)
-
-  implicit def OWLAxiomToScowlAxiom(value: OWLAxiom) = new ScowlAxiom(value)
-
-  implicit def OWLAnnotationSubjectToScowlAnnotationSubject(value: OWLAnnotationSubject) = new ScowlAnnotationSubject(value)
-
-  implicit def OWLNamedObjectToScowlNamedObject(value: OWLNamedObject) = new ScowlNamedObject(value)
-
   def not(classExpression: OWLClassExpression): OWLObjectComplementOf = OWLManager.getOWLDataFactory.getOWLObjectComplementOf(classExpression)
+
+  implicit class ScowlClassExpression(val classExpression: OWLClassExpression) extends AnyVal {
+
+    def and(other: OWLClassExpression): OWLObjectIntersectionOf = factory.getOWLObjectIntersectionOf(classExpression, other)
+
+    def or(other: OWLClassExpression): OWLObjectUnionOf = factory.getOWLObjectUnionOf(classExpression, other)
+
+    def SubClassOf(other: OWLClassExpression): OWLSubClassOfAxiom = factory.getOWLSubClassOfAxiom(classExpression, other)
+
+    def EquivalentTo(other: OWLClassExpression): OWLEquivalentClassesAxiom = factory.getOWLEquivalentClassesAxiom(classExpression, other)
+
+  }
+
+  implicit class ScowlIndividual(val self: OWLIndividual) extends AnyVal {
+
+    def Fact(property: OWLObjectProperty, value: OWLIndividual): OWLObjectPropertyAssertionAxiom = factory.getOWLObjectPropertyAssertionAxiom(property, self, value)
+
+    def Facts(facts: (OWLObjectProperty, OWLIndividual)*): Set[OWLObjectPropertyAssertionAxiom] =
+      (facts map { case (property, value) => factory.getOWLObjectPropertyAssertionAxiom(property, self, value) }).toSet
+
+    def Type(owlClass: OWLClassExpression): OWLClassAssertionAxiom = factory.getOWLClassAssertionAxiom(owlClass, self)
+
+  }
+
+  implicit class ScowlObjectProperty(val objectProperty: OWLObjectPropertyExpression) extends AnyVal {
+
+    def some(classExpression: OWLClassExpression): OWLObjectSomeValuesFrom = factory.getOWLObjectSomeValuesFrom(objectProperty, classExpression)
+
+    def only(classExpression: OWLClassExpression): OWLObjectAllValuesFrom = factory.getOWLObjectAllValuesFrom(objectProperty, classExpression)
+
+    def exactly(cardinality: Int, classExpression: OWLClassExpression): OWLObjectExactCardinality = factory.getOWLObjectExactCardinality(cardinality, objectProperty, classExpression)
+
+    def exactly(cardinality: Int): OWLObjectExactCardinality = factory.getOWLObjectExactCardinality(cardinality, objectProperty)
+
+    def min(cardinality: Int, classExpression: OWLClassExpression): OWLObjectMinCardinality = factory.getOWLObjectMinCardinality(cardinality, objectProperty, classExpression)
+
+    def min(cardinality: Int): OWLObjectMinCardinality = factory.getOWLObjectMinCardinality(cardinality, objectProperty)
+
+    def max(cardinality: Int, classExpression: OWLClassExpression): OWLObjectMaxCardinality = factory.getOWLObjectMaxCardinality(cardinality, objectProperty, classExpression)
+
+    def max(cardinality: Int): OWLObjectMaxCardinality = factory.getOWLObjectMaxCardinality(cardinality, objectProperty)
+
+    def value(individual: OWLIndividual): OWLObjectHasValue = factory.getOWLObjectHasValue(objectProperty, individual)
+
+    def o(property: OWLObjectPropertyExpression): ScowlPropertyChain = new ScowlPropertyChain(objectProperty, property)
+
+    def SubPropertyChain(chain: ScowlPropertyChain): OWLSubPropertyChainOfAxiom = factory.getOWLSubPropertyChainOfAxiom(chain.properties, objectProperty)
+
+  }
+
+  implicit class ScowlAnnotationSubject(val self: OWLAnnotationSubject) extends AnyVal {
+
+    def Annotation(property: OWLAnnotationProperty, value: OWLAnnotationValue): OWLAnnotationAssertionAxiom = {
+      factory.getOWLAnnotationAssertionAxiom(property, self, value)
+    }
+
+  }
+
+  implicit class ScowlAxiom(val self: OWLAxiom) extends AnyVal {
+
+    def Annotation(property: OWLAnnotationProperty, value: OWLAnnotationValue): OWLAxiom = {
+      self.getAnnotatedAxiom(Set(factory.getOWLAnnotation(property, value)))
+    }
+
+    def Annotation(property: OWLAnnotationProperty, value: OWLNamedObject): OWLAxiom = {
+      self.getAnnotatedAxiom(Set(factory.getOWLAnnotation(property, value.getIRI)))
+    }
+
+  }
+
+  implicit class ScowlNamedObject(val self: OWLNamedObject) extends AnyVal {
+
+    def Annotation(property: OWLAnnotationProperty, value: String): OWLAnnotationAssertionAxiom = Annotation(property, factory.getOWLLiteral(value))
+
+    def Annotation(property: OWLAnnotationProperty, value: OWLAnnotationValue): OWLAnnotationAssertionAxiom = factory.getOWLAnnotationAssertionAxiom(property, self.getIRI, value)
+
+  }
 
 }
