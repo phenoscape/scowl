ce28be096e9c49a01f085ff0791d50424fd804dc
Jim Balhoff <jim@balhoff.org>
2016-02-03 14:58:11 -0500

    Fixed reversal of min and max facets.

--- src/main/scala/org/phenoscape/scowl/OWL.scala
+++ src/main/scala/org/phenoscape/scowl/OWL.scala
@@ -158,37 +158,37 @@ object OWL {
 
   }
 
-  def <(value: OWLLiteral) = factory.getOWLFacetRestriction(OWLFacet.MIN_EXCLUSIVE, value)
+  def <(value: OWLLiteral) = factory.getOWLFacetRestriction(OWLFacet.MAX_EXCLUSIVE, value)
 
-  def <=(value: OWLLiteral) = factory.getOWLFacetRestriction(OWLFacet.MIN_INCLUSIVE, value)
+  def <=(value: OWLLiteral) = factory.getOWLFacetRestriction(OWLFacet.MAX_INCLUSIVE, value)
 
-  def >(value: OWLLiteral) = factory.getOWLFacetRestriction(OWLFacet.MAX_EXCLUSIVE, value)
+  def >(value: OWLLiteral) = factory.getOWLFacetRestriction(OWLFacet.MIN_EXCLUSIVE, value)
 
-  def >=(value: OWLLiteral) = factory.getOWLFacetRestriction(OWLFacet.MAX_INCLUSIVE, value)
+  def >=(value: OWLLiteral) = factory.getOWLFacetRestriction(OWLFacet.MIN_INCLUSIVE, value)
 
-  def <(value: Int) = factory.getOWLFacetRestriction(OWLFacet.MIN_EXCLUSIVE, value)
+  def <(value: Int) = factory.getOWLFacetRestriction(OWLFacet.MAX_EXCLUSIVE, value)
 
-  def <=(value: Int) = factory.getOWLFacetRestriction(OWLFacet.MIN_INCLUSIVE, value)
+  def <=(value: Int) = factory.getOWLFacetRestriction(OWLFacet.MAX_INCLUSIVE, value)
 
-  def >(value: Int) = factory.getOWLFacetRestriction(OWLFacet.MAX_EXCLUSIVE, value)
+  def >(value: Int) = factory.getOWLFacetRestriction(OWLFacet.MIN_EXCLUSIVE, value)
 
-  def >=(value: Int) = factory.getOWLFacetRestriction(OWLFacet.MAX_INCLUSIVE, value)
+  def >=(value: Int) = factory.getOWLFacetRestriction(OWLFacet.MIN_INCLUSIVE, value)
 
-  def <(value: Float) = factory.getOWLFacetRestriction(OWLFacet.MIN_EXCLUSIVE, value)
+  def <(value: Float) = factory.getOWLFacetRestriction(OWLFacet.MAX_EXCLUSIVE, value)
 
-  def <=(value: Float) = factory.getOWLFacetRestriction(OWLFacet.MIN_INCLUSIVE, value)
+  def <=(value: Float) = factory.getOWLFacetRestriction(OWLFacet.MAX_INCLUSIVE, value)
 
-  def >(value: Float) = factory.getOWLFacetRestriction(OWLFacet.MAX_EXCLUSIVE, value)
+  def >(value: Float) = factory.getOWLFacetRestriction(OWLFacet.MIN_EXCLUSIVE, value)
 
-  def >=(value: Float) = factory.getOWLFacetRestriction(OWLFacet.MAX_INCLUSIVE, value)
+  def >=(value: Float) = factory.getOWLFacetRestriction(OWLFacet.MIN_INCLUSIVE, value)
 
-  def <(value: Double) = factory.getOWLFacetRestriction(OWLFacet.MIN_EXCLUSIVE, value)
+  def <(value: Double) = factory.getOWLFacetRestriction(OWLFacet.MAX_EXCLUSIVE, value)
 
-  def <=(value: Double) = factory.getOWLFacetRestriction(OWLFacet.MIN_INCLUSIVE, value)
+  def <=(value: Double) = factory.getOWLFacetRestriction(OWLFacet.MAX_INCLUSIVE, value)
 
-  def >(value: Double) = factory.getOWLFacetRestriction(OWLFacet.MAX_EXCLUSIVE, value)
+  def >(value: Double) = factory.getOWLFacetRestriction(OWLFacet.MIN_EXCLUSIVE, value)
 
-  def >=(value: Double) = factory.getOWLFacetRestriction(OWLFacet.MAX_INCLUSIVE, value)
+  def >=(value: Double) = factory.getOWLFacetRestriction(OWLFacet.MIN_INCLUSIVE, value)
 
   implicit class ScowlIndividual(val self: OWLIndividual) extends AnyVal {
 
