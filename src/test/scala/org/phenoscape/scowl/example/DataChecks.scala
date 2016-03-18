716bf5ac22a4819de798ffc2c9accab7b440f07a
Jim Balhoff <jim@balhoff.org>
2016-03-18 14:19:34 -0400

    Updated example data checks.

--- src/test/scala/org/phenoscape/scowl/example/DataChecks.scala
+++ src/test/scala/org/phenoscape/scowl/example/DataChecks.scala
@@ -17,26 +17,45 @@ class DataChecks extends UnitSpec {
 
   val manager = OWLManager.createOWLOntologyManager()
   val ont = manager.loadOntology(IRI.create("http://purl.obolibrary.org/obo/uberon/ext.owl"))
+  val HasExactSynonym = AnnotationProperty("http://www.geneontology.org/formats/oboInOwl#hasExactSynonym")
+  val HasNarrowSynonym = AnnotationProperty("http://www.geneontology.org/formats/oboInOwl#hasNarrowSynonym")
+  val HasBroadSynonym = AnnotationProperty("http://www.geneontology.org/formats/oboInOwl#hasBroadSynonym")
+  val HasRelatedSynonym = AnnotationProperty("http://www.geneontology.org/formats/oboInOwl#hasRelatedSynonym")
+  val synonymProperties = Set(HasExactSynonym, HasNarrowSynonym, HasBroadSynonym, HasRelatedSynonym)
 
   "rdfs:label values" should "be used only once per ontology" in {
     ont.getAxioms(Imports.EXCLUDED)
       .collect { case axiom @ AnnotationAssertion(_, RDFSLabel, _, value ^^ _) => Map(value -> Set(axiom)) }
       .reduce(_ |+| _)
-      .filter { case (label, annotations) => annotations.size > 1 }
-      .isEmpty should be(true)
+      .filter { case (label, annotations) => annotations.size > 1 } shouldBe empty
+  }
+
+  "hasExactSynonym values" should "be used only once per ontology" in {
+    ont.getAxioms(Imports.EXCLUDED)
+      .collect { case axiom @ AnnotationAssertion(_, HasExactSynonym, _, value ^^ _) => Map(value -> Set(axiom)) }
+      .reduce(_ |+| _)
+      .filter { case (label, annotations) => annotations.size > 1 } shouldBe empty
+  }
+
+  // This test is very slow; SPARQL would probably be better (perhaps for many other such tests as well).
+  "No class" should "have a synonym that matches the label" ignore { //FIXME ignored 
+    val annotations = ont.getAxioms(AxiomType.ANNOTATION_ASSERTION, Imports.EXCLUDED).toSet
+    (for {
+      AnnotationAssertion(_, RDFSLabel, termIRI, label ^^ _) <- annotations
+      AnnotationAssertion(_, synonymProperty, `termIRI`, `label` ^^ _) <- annotations
+      if synonymProperties(synonymProperty)
+    } yield termIRI -> label) shouldBe empty
   }
 
   "Annotation values" should "not contain double spaces" in {
     ont.getAxioms(Imports.EXCLUDED)
-      .collect { case axiom @ AnnotationAssertion(_, _, _, value ^^ _) if value.contains("  ") => axiom }
-      .isEmpty should be(true)
+      .collect { case axiom @ AnnotationAssertion(_, _, _, value ^^ _) if value.contains("  ") => axiom } shouldBe empty
 
   }
 
   "Trim" should "not have an effect on annotation values" in {
     ont.getAxioms(Imports.EXCLUDED)
-      .collect { case axiom @ AnnotationAssertion(_, _, _, value ^^ _) if value.trim != value => axiom }
-      .isEmpty should be(true)
+      .collect { case axiom @ AnnotationAssertion(_, _, _, value ^^ _) if value.trim != value => axiom } shouldBe empty
   }
 
 }
