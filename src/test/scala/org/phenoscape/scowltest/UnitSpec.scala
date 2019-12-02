b94dfe61374472122a9f3fc54051c2c950711a11
Scala Steward <me@scala-steward.org>
2019-12-02 08:30:12 +0100

    Update scalatest to 3.1.0

--- src/test/scala/org/phenoscape/scowltest/UnitSpec.scala
+++ src/test/scala/org/phenoscape/scowltest/UnitSpec.scala
@@ -1,6 +1,8 @@
 package org.phenoscape.scowltest
 
 import org.scalatest._
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
 
-abstract class UnitSpec extends FlatSpec with Matchers with OptionValues with Inside with Inspectors
+abstract class UnitSpec extends AnyFlatSpec with Matchers with OptionValues with Inside with Inspectors
   
