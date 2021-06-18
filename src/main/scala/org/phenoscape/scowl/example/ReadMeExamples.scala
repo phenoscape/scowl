e293e59dbcf181bce17149f27ec63df35c2892a2
Jim Balhoff <jim@balhoff.org>
2021-06-18 15:50:09 -0400

    Add missing recursion in NNF example.

--- src/main/scala/org/phenoscape/scowl/example/ReadMeExamples.scala
+++ src/main/scala/org/phenoscape/scowl/example/ReadMeExamples.scala
@@ -62,24 +62,24 @@ object ReadMeExamples {
    * Using pattern matching extractors to implement negation normal form
    */
   def nnf(expression: OWLClassExpression): OWLClassExpression = expression match {
-    case Class(_) => expression
-    case ObjectComplementOf(Class(_)) => expression
-    case ObjectComplementOf(ObjectComplementOf(expression)) => nnf(expression)
-    case ObjectUnionOf(operands) => ObjectUnionOf(operands.map(nnf))
-    case ObjectIntersectionOf(operands) => ObjectIntersectionOf(operands.map(nnf))
-    case ObjectComplementOf(ObjectUnionOf(operands)) => ObjectIntersectionOf(operands.map(c => nnf(ObjectComplementOf(c))))
-    case ObjectComplementOf(ObjectIntersectionOf(operands)) => ObjectUnionOf(operands.map(c => nnf(ObjectComplementOf(c))))
-    case ObjectAllValuesFrom(property, filler) => ObjectAllValuesFrom(property, nnf(filler))
-    case ObjectSomeValuesFrom(property, filler) => ObjectSomeValuesFrom(property, nnf(filler))
-    case ObjectMinCardinality(num, property, filler) => ObjectMinCardinality(num, property, nnf(filler))
-    case ObjectMaxCardinality(num, property, filler) => ObjectMaxCardinality(num, property, nnf(filler))
-    case ObjectExactCardinality(num, property, filler) => ObjectExactCardinality(num, property, nnf(filler))
-    case ObjectComplementOf(ObjectAllValuesFrom(property, filler)) => ObjectSomeValuesFrom(property, nnf(ObjectComplementOf(filler)))
-    case ObjectComplementOf(ObjectSomeValuesFrom(property, filler)) => ObjectAllValuesFrom(property, nnf(ObjectComplementOf(filler)))
-    case ObjectComplementOf(ObjectMinCardinality(num, property, filler)) => ObjectMaxCardinality(math.max(num - 1, 0), property, filler)
-    case ObjectComplementOf(ObjectMaxCardinality(num, property, filler)) => ObjectMinCardinality(num + 1, property, filler)
-    case ObjectComplementOf(ObjectExactCardinality(num, property, filler)) => ObjectUnionOf(ObjectMinCardinality(num + 1, property, filler), ObjectMaxCardinality(math.max(num - 1, 0), property, filler))
-    case _ => ???
+    case Class(_)                                                          => expression
+    case ObjectComplementOf(Class(_))                                      => expression
+    case ObjectComplementOf(ObjectComplementOf(expression))                => nnf(expression)
+    case ObjectUnionOf(operands)                                           => ObjectUnionOf(operands.map(nnf))
+    case ObjectIntersectionOf(operands)                                    => ObjectIntersectionOf(operands.map(nnf))
+    case ObjectComplementOf(ObjectUnionOf(operands))                       => ObjectIntersectionOf(operands.map(c => nnf(ObjectComplementOf(c))))
+    case ObjectComplementOf(ObjectIntersectionOf(operands))                => ObjectUnionOf(operands.map(c => nnf(ObjectComplementOf(c))))
+    case ObjectAllValuesFrom(property, filler)                             => ObjectAllValuesFrom(property, nnf(filler))
+    case ObjectSomeValuesFrom(property, filler)                            => ObjectSomeValuesFrom(property, nnf(filler))
+    case ObjectMinCardinality(num, property, filler)                       => ObjectMinCardinality(num, property, nnf(filler))
+    case ObjectMaxCardinality(num, property, filler)                       => ObjectMaxCardinality(num, property, nnf(filler))
+    case ObjectExactCardinality(num, property, filler)                     => ObjectExactCardinality(num, property, nnf(filler))
+    case ObjectComplementOf(ObjectAllValuesFrom(property, filler))         => ObjectSomeValuesFrom(property, nnf(ObjectComplementOf(filler)))
+    case ObjectComplementOf(ObjectSomeValuesFrom(property, filler))        => ObjectAllValuesFrom(property, nnf(ObjectComplementOf(filler)))
+    case ObjectComplementOf(ObjectMinCardinality(num, property, filler))   => ObjectMaxCardinality(math.max(num - 1, 0), property, nnf(filler))
+    case ObjectComplementOf(ObjectMaxCardinality(num, property, filler))   => ObjectMinCardinality(num + 1, property, nnf(filler))
+    case ObjectComplementOf(ObjectExactCardinality(num, property, filler)) => ObjectUnionOf(ObjectMinCardinality(num + 1, property, nnf(filler)), ObjectMaxCardinality(math.max(num - 1, 0), property, nnf(filler)))
+    case _                                                                 => ???
   }
 
   /**
